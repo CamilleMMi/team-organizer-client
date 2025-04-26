b6a5d602b6ca700c435b7329c2364dca8f067610
CamilleMMi <camillemichaudmeli@gmail.com>
2025-04-26 10:29:22 +0200

    Identation

--- src/features/page-meta/components/page-meta-updater.component.1.0.0.jsx
+++ src/features/page-meta/components/page-meta-updater.component.1.0.0.jsx
@@ -4,16 +4,16 @@ import { useDynamicTitle } from "../hooks/use-dynamic-title.hook.1.0.0";
 import { useFavicon } from "../hooks/use-favicon.hook.1.0.0";
 
 function PageMetaUpdater() {
-  const location = useLocation();
-  const matchMessageDetail = useMatch("/messages/:id");
-  const id = matchMessageDetail?.params?.id;
+    const location = useLocation();
+    const matchMessageDetail = useMatch("/messages/:id");
+    const id = matchMessageDetail?.params?.id;
 
-  const clientInfos = useClientInfos();
+    const clientInfos = useClientInfos();
 
-  useDynamicTitle(clientInfos, location.pathname, id);
-  useFavicon(clientInfos?.logoUrl);
+    useDynamicTitle(clientInfos, location.pathname, id);
+    useFavicon(clientInfos?.logoUrl);
 
-  return null;
+    return null;
 }
 
 export default PageMetaUpdater;
